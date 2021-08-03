88f02210338b62a63921f58768d5cb2ef6bc5e00
daviddcho <amazingdcho@gmail.com>
2021-08-03 15:47:57 -0400

    added basic corner pts

--- slam.py
+++ slam.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import cv2
+import numpy as np
 from display import Display3D
+from frame import Frame
 import time
 
 if __name__ == "__main__":
@@ -27,6 +29,14 @@ if __name__ == "__main__":
       # then paint
     else:
       break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    pts = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
+    pts = np.int0(pts)
+    for f in pts:
+      #print(f[0][0], f[0][1])
+      x,y = f.ravel()
+      cv2.circle(frame, (x,y), 3, 255, -1)
 
     time.sleep(.01)    
     disp3d.paint(frame)
